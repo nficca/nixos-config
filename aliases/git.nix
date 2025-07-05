39218bd4f5472701ba5e21c23615c12b7d4314b9
Nic Ficca <nicficca@gmail.com>
2025-07-05 16:46:10 -0400

    Add git diff alias

--- aliases/git.nix
+++ aliases/git.nix
@@ -3,6 +3,7 @@
   gc = "git commit --verbose";
   gcb = "git checkout -b";
   gco = "git checkout";
+  gd = "git diff";
   gl = "git pull";
   gp = "git push";
   gpsup = "git push --set-upstream origin $(git rev-parse --abbrev-ref HEAD)";
