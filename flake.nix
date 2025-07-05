b8784b5b4a7ca456b058b37183577bc1f2996c3e
Nic Ficca <nicficca@gmail.com>
2025-07-05 12:36:55 -0400

    Switch to unstable branch for nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "Nic's NixOS Flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     # Manage user configuration with home-manager.
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
