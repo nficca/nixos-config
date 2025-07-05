f58f69f1238de956d069c456565bf3e6e14dd8df
Nic Ficca <nicficca@gmail.com>
2025-07-05 16:42:53 -0400

    Add pipe-operators feature

--- configuration.nix
+++ configuration.nix
@@ -3,7 +3,6 @@
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 {
-  config,
   pkgs,
   globals,
   ...
@@ -123,6 +122,7 @@
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
+    "pipe-operators"
   ];
 
   # List packages installed in system profile.
