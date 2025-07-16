b494236dbcb163046836aad151ff21ba3271aed1
Nic Ficca <nic@fossa.com>
2025-07-16 09:50:25 -0400

    Add claude code

--- hosts/darwin/home.nix
+++ hosts/darwin/home.nix
@@ -1,4 +1,4 @@
-{ common, ... }:
+{ common, pkgs, ... }:
 
 {
   imports = [
@@ -6,4 +6,9 @@
   ];
 
   home.username = common.username;
+
+  # Install packages
+  home.packages = with pkgs; [
+    claude-code # Agentic AI assistant
+  ];
 }
