6702e5c5a5ec82954155b8b23b02bee63666a0dc
Nic Ficca <nicficca@gmail.com>
2025-07-22 19:43:47 -0400

    Add glo alias

--- shared/home/aliases.nix
+++ shared/home/aliases.nix
@@ -9,6 +9,7 @@
     gco = "git checkout";
     gd = "git diff";
     gl = "git pull";
+    glo = "git log --oneline";
     gp = "git push";
     gpsup = "git push --set-upstream origin $(git rev-parse --abbrev-ref HEAD)";
     gst = "git status";
