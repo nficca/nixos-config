bf66e5e41dde9397ff5299bd70b3639c6ecc3889
Nic Ficca <nicficca@gmail.com>
2025-08-19 22:42:50 -0400

    Add nil to lsp config (lua lsp)

--- home/dotfiles/nvim/lua/plugins.lua
+++ home/dotfiles/nvim/lua/plugins.lua
@@ -117,9 +117,8 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
-      require('lspconfig').rust_analyzer.setup({
-        capabilities = capabilities,
-      })
+      require('lspconfig').rust_analyzer.setup({ capabilities = capabilities })
+      require('lspconfig').nil_ls.setup({ capabilities = capabilities })
       vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, { desc = "Rename symbol" })
     end
   },
