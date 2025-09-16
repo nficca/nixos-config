b2bb81a9632c941c1ecd2423fb0696b685536916
Nic Ficca <nic@fossa.com>
2025-09-16 10:59:37 -0400

    Use telescope-frecency

--- shared/home/dotfiles/nvim/lua/plugins/telescope.lua
+++ shared/home/dotfiles/nvim/lua/plugins/telescope.lua
@@ -1,18 +1,21 @@
 return {
   "nvim-telescope/telescope.nvim",
   tag = "0.1.8",
-  dependencies = { "nvim-lua/plenary.nvim" },
+  dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-frecency.nvim" },
   config = function()
-    require("telescope").setup({
+    local telescope = require("telescope")
+
+    telescope.load_extension("frecency")
+    telescope.setup({
       defaults = {
         layout_strategy = "horizontal",
-        layout_config = { width = 0.9, height = 0.9, prompt_position = "top" },
+        layout_config = { prompt_position = "top" },
         sorting_strategy = "ascending"
       },
     })
   end,
   keys = {
-    { "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "Find files" },
+    { "<leader>ff", "<cmd>Telescope frecency workspace=CWD<CR>", desc = "Find files" },
     { "<leader>fg", "<cmd>Telescope live_grep<CR>",  desc = "Find text in files" }
   }
 }
