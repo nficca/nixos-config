2e1cd351aee2d3b17fe2964918ed68e02fbf1030
Nic Ficca <nicficca@gmail.com>
2025-12-15 13:42:57 -0500

    Redesign lualine

--- shared/home/dotfiles/nvim/lua/plugins/lualine.lua
+++ shared/home/dotfiles/nvim/lua/plugins/lualine.lua
@@ -2,20 +2,32 @@ return {
   "nvim-lualine/lualine.nvim",
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   opts = {
+    -- Sections:
+    -- +-------------------------------------------------+
+    -- | A | B | C                             X | Y | Z |
+    -- +-------------------------------------------------+
     sections = {
-      lualine_c = {
+      lualine_a = { "mode" },
+      lualine_b = {
+        "branch",
         {
-          'filename',
-          path = 1
-        }
+          "filetype",
+          colored = true,
+          icon_only = true,
+          separator = "",
+          padding = { left = 1, right = 0 }
+        },
+        {
+          "filename",
+          path = 1,
+          file_status = false,
+          padding = { left = 0, right = 1 }
+        },
       },
-      lualine_x = {
-        {
-          'diagnostics',
-          sources = { 'nvim_diagnostic' },
-          symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' }
-        }
-      }
+      lualine_c = { "location" },
+      lualine_x = {},
+      lualine_y = { "diagnostics" },
+      lualine_z = { "lsp_status" }
     }
   }
 }
