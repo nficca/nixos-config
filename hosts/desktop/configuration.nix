6608d98ad57f9d9b4cb082b1273d34fcdeec76fa
Nic Ficca <nicficca@gmail.com>
2026-02-04 08:27:43 -0500

    Add blueman

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -29,6 +29,7 @@
   # Enable bluetooth
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
+  services.blueman.enable = true;
 
   # Set your time zone.
   time.timeZone = "America/Toronto";
