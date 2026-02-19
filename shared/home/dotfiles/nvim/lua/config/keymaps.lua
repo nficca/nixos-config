6dd300e786a0b860c5e6be815859a11f814df45b
Nic Ficca <nicficca@gmail.com>
2026-02-19 12:33:52 -0500

    Add more clipboard keymaps

--- shared/home/dotfiles/nvim/lua/config/keymaps.lua
+++ shared/home/dotfiles/nvim/lua/config/keymaps.lua
@@ -5,7 +5,7 @@ vim.keymap.set("n", "<leader>?", function()
 end, { desc = "Show local keymaps" })
 
 -- Language server keymaps --
-which_key.add({ "<leader>l", group = "Language server tools"})
+which_key.add({ "<leader>l", group = "Language server tools" })
 
 vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, { desc = "Rename symbol" })
 vim.keymap.set("n", "<leader>ld", vim.lsp.buf.definition, { desc = "Jump to definition" })
@@ -16,7 +16,7 @@ vim.keymap.set("n", "<leader>lh", function()
   vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
 end, { desc = "Toggle inlay hints" })
 
-which_key.add({ "<leader>lx", group = "Diagnostics"})
+which_key.add({ "<leader>lx", group = "Diagnostics" })
 
 -- Toggles diagnostics between:
 --   - separate lines (virtual_lines)
@@ -89,7 +89,7 @@ telescope_keymap("fT", function()
     end,
   })
 end, "Find text (literal)")
-telescope_keymap("fb", function() telescope.buffers({ sort_lastused = true}) end, "Find opened buffers")
+telescope_keymap("fb", function() telescope.buffers({ sort_lastused = true }) end, "Find opened buffers")
 telescope_keymap("fr", telescope.lsp_references, "Find references of symbol")
 telescope_keymap("fg", telescope.git_status, "Find git changes")
 telescope_keymap("fs", telescope.lsp_dynamic_workspace_symbols, "Find symbols")
@@ -102,11 +102,27 @@ telescope_keymap("F", telescope.resume, "Resume previous search")
 -- Yank keymaps --
 which_key.add({ "<leader>y", group = "Yank/Copy" })
 
--- System clipboard yank (copy)
-vim.keymap.set("v", "<leader>yy", '"+y', { desc = "Yank to system clipboard" })
-vim.keymap.set("n", "<leader>yy", '"+yy', { desc = "Yank line to system clipboard" })
+local function truncate(text, max_len)
+  max_len = max_len or 50
+  local single_line = text:gsub("\n", "\\n")
+  if #single_line > max_len then
+    return single_line:sub(1, max_len) .. "..."
+  end
+  return single_line
+end
 
--- Gitlinker (copy git permalink)
+-- Yank to system clipboard
+vim.keymap.set("v", "<leader>yy", function()
+  vim.cmd('normal! "+y')
+  print("Yanked: " .. truncate(vim.fn.getreg("+")))
+end, { desc = "Yank to system clipboard" })
+
+vim.keymap.set("n", "<leader>yy", function()
+  vim.cmd('normal! "+yy')
+  print("Yanked: " .. truncate(vim.fn.getreg("+")))
+end, { desc = "Yank line to system clipboard" })
+
+-- Copy git remote permalink to system clipboard
 local gitlinker = require("gitlinker")
 vim.keymap.set("n", "<leader>yg", function()
   gitlinker.get_buf_range_url("n")
@@ -115,24 +131,45 @@ vim.keymap.set("v", "<leader>yg", function()
   gitlinker.get_buf_range_url("v")
 end, { desc = "Copy git permalink (selection)" })
 
+-- Copy file path to clipboard
+vim.keymap.set("n", "<leader>yf", function()
+  local filepath = vim.fn.expand("%")
+  vim.fn.setreg("+", filepath)
+  print("Copied: " .. filepath)
+end, { desc = "Copy file path" })
+
+-- Copy file:line to clipboard
+vim.keymap.set("n", "<leader>yl", function()
+  local file_line = vim.fn.expand("%") .. ":" .. vim.fn.line(".")
+  vim.fn.setreg("+", file_line)
+  print("Copied: " .. file_line)
+end, { desc = "Copy file:line" })
+
+vim.keymap.set("v", "<leader>yl", function()
+  local start_line = vim.fn.line("v")
+  local end_line = vim.fn.line(".")
+  if start_line > end_line then
+    start_line, end_line = end_line, start_line
+  end
+  local file_range = vim.fn.expand("%") .. ":" .. start_line .. "-" .. end_line
+  vim.fn.setreg("+", file_range)
+  print("Copied: " .. file_range)
+end, { desc = "Copy file:line-range" })
+
 -- System clipboard paste
-vim.keymap.set({"n", "v"}, "<leader>p", '"+p', { desc = "Paste from system clipboard" })
-vim.keymap.set({"n", "v"}, "<leader>P", '"+P', { desc = "Paste before from system clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', { desc = "Paste from system clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>P", '"+P', { desc = "Paste before from system clipboard" })
 
 -- Delete without yanking (black-hole register)
-vim.keymap.set({"n", "v"}, "<leader>d", '"_d', { desc = "Delete without yanking" })
+vim.keymap.set({ "n", "v" }, "<leader>d", '"_d', { desc = "Delete without yanking" })
 vim.keymap.set("n", "<leader>dd", '"_dd', { desc = "Delete line without yanking" })
 
 -- Change without yanking (black-hole register)
-vim.keymap.set({"n", "v"}, "<leader>c", '"_c', { desc = "Change without yanking" })
+vim.keymap.set({ "n", "v" }, "<leader>c", '"_c', { desc = "Change without yanking" })
 vim.keymap.set("n", "<leader>cc", '"_cc', { desc = "Change line without yanking" })
 
 -- Safe visual paste (preserves yank register)
 vim.keymap.set("v", "<leader>vp", '"_dP', { desc = "Paste without overwriting register" })
-
--- Cut to system clipboard (delete + yank)
-vim.keymap.set({"n", "v"}, "<leader>x", '"+d', { desc = "Cut to system clipboard" })
-vim.keymap.set("n", "<leader>xx", '"+dd', { desc = "Cut line to system clipboard" })
 
 -- Tree sitter keymaps --
 -- Tree sitter keymaps are in the plugin configuration for treesitter.
