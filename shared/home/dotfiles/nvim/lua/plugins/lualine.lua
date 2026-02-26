9668c1c5666870a23e56e0de5b7ab6ecb467be6e
Nic Ficca <nicficca@gmail.com>
2026-02-26 14:14:23 -0500

    Improve lualine

--- shared/home/dotfiles/nvim/lua/plugins/lualine.lua
+++ shared/home/dotfiles/nvim/lua/plugins/lualine.lua
@@ -2,6 +2,11 @@ return {
   "nvim-lualine/lualine.nvim",
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   opts = {
+    options = {
+      theme = "ayu_dark",
+      component_separators = { left = '', right = '' },
+      section_separators = { left = '', right = '' },
+    },
     -- Sections:
     -- +-------------------------------------------------+
     -- | A | B | C                             X | Y | Z |
@@ -10,22 +15,29 @@ return {
       lualine_a = { "mode" },
       lualine_b = {
         "branch",
+
         {
           "filetype",
           colored = true,
           icon_only = true,
           separator = "",
-          padding = { left = 1, right = 0 }
+          padding = { left = 0, right = 0 }
         },
         {
           "filename",
           path = 1,
-          file_status = false,
-          padding = { left = 0, right = 1 }
+          file_status = true,
+          padding = { left = 0, right = 0 },
+          symbols = {
+            modified = '', -- Text to show when the file is modified.
+            readonly = '', -- Text to show when the file is non-modifiable or readonly.
+            unnamed = '', -- Text to show for unnamed buffers.
+            newfile = '', -- Text to show for newly created file bef
+          }
         },
       },
-      lualine_c = { "location" },
-      lualine_x = {},
+      lualine_c = {},
+      lualine_x = { "location" },
       lualine_y = { "diagnostics" },
       lualine_z = { "lsp_status" }
     }
