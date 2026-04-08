632f0293846d9c62b0769a7dc064bd218ff01837
Nic Ficca <nicficca@gmail.com>
2026-04-07 21:20:36 -0400

    Fix conflicting keymap

--- shared/home/dotfiles/nvim/lua/config/keymaps.lua
+++ shared/home/dotfiles/nvim/lua/config/keymaps.lua
@@ -42,7 +42,7 @@ vim.keymap.set("n", "<leader>dt", "<cmd>Trouble diagnostics toggle<cr>", { desc 
 -- Hoversplit allow for LSP hover text in a split window
 -- Since it's LSP related, its keymaps can share the same prefix
 local hoversplit = require("hoversplit")
-vim.keymap.set("n", "<leader>lh", hoversplit.split_remain_focused, { desc = "Toggle LSP split (horizontal)" })
+vim.keymap.set("n", "<leader>h", hoversplit.split_remain_focused, { desc = "Toggle hoversplit" })
 
 
 -- Neogit keymaps --
