b4cae0a9fa49eab0d0ab6265b972cdb20b28031b
Nic Ficca <nicficca@gmail.com>
2026-04-07 21:28:12 -0400

    Use nvim-web-devicons in oil

--- shared/home/dotfiles/nvim/lua/plugins/oil.lua
+++ shared/home/dotfiles/nvim/lua/plugins/oil.lua
@@ -12,8 +12,7 @@ return {
     }
   },
   -- Optional dependencies
-  dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+  dependencies = { { "nvim-tree/nvim-web-devicons", opts = {} } },
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
 }
