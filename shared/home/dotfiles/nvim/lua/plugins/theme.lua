bfa4a989ce0448f5cf0348ea7699f440cc4e014d
Nic Ficca <nicficca@gmail.com>
2026-04-08 08:21:01 -0400

    Correctly specify lazy for theme

--- shared/home/dotfiles/nvim/lua/plugins/theme.lua
+++ shared/home/dotfiles/nvim/lua/plugins/theme.lua
@@ -1,11 +1,11 @@
 return {
   "projekt0n/github-nvim-theme",
   name = "github-theme",
+  lazy = false,
   config = function()
     require("github-theme").setup({
       options = {
         transparent = true,
-        lazy = false
       }
     })
   end
