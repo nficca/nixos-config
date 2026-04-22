e563002d6f4cf261a627ccaf4503c95c824dc18b
Nic Ficca <nicficca@gmail.com>
2026-04-22 17:18:01 -0400

    Set Host header on proxy

--- hosts/hetzner/calibre-web.nix
+++ hosts/hetzner/calibre-web.nix
@@ -52,6 +52,7 @@ in
       proxyPass = "http://127.0.0.1:8083";
       proxyWebsockets = true;
       extraConfig = ''
+        proxy_set_header Host $host;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto $scheme;
         proxy_set_header X-Script-Name "";
