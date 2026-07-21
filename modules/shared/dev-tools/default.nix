cf24e4a71fdb2835750dc055aabab16172588b1a
Nic Ficca <nicficca@gmail.com>
2026-07-21 10:41:06 -0400

    Revert "Add herdr"
    This reverts commit f662d23b7bd13f80ab4730e2cad7681f74e9dce2.

--- modules/shared/dev-tools/default.nix
+++ modules/shared/dev-tools/default.nix
@@ -16,7 +16,6 @@
         clang-tools # clangd LSP + clang-format
         cmake-language-server
         hunk # Terminal diff viewer for agentic changesets
-        herdr # Agent multiplexer that lives in your terminal
         kdePackages.qtdeclarative # QML language tooling
         lua-language-server
         nil # Nix LSP
