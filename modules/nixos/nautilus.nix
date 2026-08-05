aea691edf815c3b4882f4d0bba5406549a944f2e
Nic Ficca <nicficca@gmail.com>
2026-08-05 13:16:39 -0400

    Enable gvfs so Nautilus can open trash:// and network://

--- modules/nixos/nautilus.nix
+++ modules/nixos/nautilus.nix
@@ -10,6 +10,11 @@
   options.myModules.nautilus.enable = lib.mkEnableOption "nautilus (GNOME file manager) registered as the default handler for inode/directory";
 
   config = lib.mkIf config.myModules.nautilus.enable {
+    # Nautilus reaches the trash, network shares and MTP devices through gvfs
+    # URI backends. Without it, opening Trash fails with
+    # '"trash" locations are not supported'.
+    services.gvfs.enable = true;
+
     home-manager.users.${username} = {
       home.packages = [ pkgs.nautilus ];
 
